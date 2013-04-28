c4dae8a57e62564ac3da4a6959887db38882b28e
Hristian Dimov <hristian.dimov@gmail.com>
2013-04-28 20:46:35 +0300

    05. UsingVariables homework [HQC]

---
+++ HighQualityCode/05. UsingVariables/01. Size/Size.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+public class Size
+{
+    private double width;
+    private double height;
+
+    public Size(double width, double height)
+    {
+        this.width = width;
+        this.height = height;
+    }
+
+    public double Width
+    {
+        get { return width; }
+        set { width = value; }
+    }
+
+    public double Height
+    {
+        get { return height; }
+        set { height = value; }
+    }
+
+
+    public static Size GetRotatedSize(Size size, double angleOfRotation)
+    {
+        double cosWidth = Math.Abs(Math.Cos(angleOfRotation)) * size.Width;
+        double cosHeight = Math.Abs(Math.Cos(angleOfRotation)) * size.Height;
+        double sinWidth = Math.Abs(Math.Sin(angleOfRotation)) * size.Height;
+        double sinHeight = Math.Abs(Math.Sin(angleOfRotation)) * size.Width;
+
+        double width = cosWidth + sinWidth;
+        double height = cosHeight + sinHeight;
+
+        Size rotatedSize = new Size(width, height);
+
+        return rotatedSize;
+    }
+}
---
+++ HighQualityCode/05. UsingVariables/02. PrintStatistics/PrintStats.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+class PrintStats
+{
+    public void PrintStatistics(double[] numbers, int count)
+    {
+        double maxNumber = FindMax(numbers);
+        Console.WriteLine("This is max number: {0}", maxNumber);
+
+        double minNumber = FindMin(numbers);
+        Console.WriteLine("This is min number: {0}", minNumber);
+
+        double averageNumber = FindAverage(numbers);
+        Console.WriteLine("This is avg number: {0}", averageNumber);
+    }
+
+    private static double FindAverage(double[] numbers)
+    {
+        double sum = 0;
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            sum += numbers[i];
+        }
+
+        double average = sum / numbers.Length;
+        return average;
+    }
+
+    private static double FindMin(double[] numbers)
+    {
+        double min = numbers[0];
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            if (numbers[i] < min)
+            {
+                min = numbers[i];
+            }
+        }
+        return min;
+    }
+
+    private static double FindMax(double[] numbers)
+    {
+        double max = numbers[0];
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            if (numbers[i] > max)
+            {
+                max = numbers[i];
+            }
+        }
+
+        return max;
+    }
+
+    static void Main()
+    {
+        PrintStats stats = new PrintStats();
+        stats.PrintStatistics(new double[3] { 1, 2, 6 }, 3);
+    }
+}
