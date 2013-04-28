37034749e0e6c0393c33d41e2f99231e2cb5971f
Hristian Dimov <hristian.dimov@gmail.com>
2013-04-28 21:02:07 +0300

    06. UsingControlStructures homework [HQC]

---
+++ HighQualityCode/06. UsingControlStructures/01. Kitchen/Chef.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Chef
+{
+    public void Cook()
+    {
+        Bowl bowl = GetBowl();
+
+        Carrot carrot = GetCarrot();
+        Peel(carrot);
+        Cut(carrot);
+        bowl.Add(carrot);
+
+        Potato potato = GetPotato();
+        Peel(potato);
+        Cut(potato);
+        bowl.Add(potato);
+    }
+
+    private Bowl GetBowl()
+    {
+    }
+
+    private Carrot GetCarrot()
+    {
+    }
+
+    private Potato GetPotato()
+    {
+    }
+
+    private void Cut(Vegetable vegetable)
+    {
+    }
+}
---
+++ HighQualityCode/06. UsingControlStructures/02. RefactorIfStatements/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Potato potato;
+
+        if (potato != null)
+        {
+            if (!potato.HasNotBeenPeeled && !potato.IsRotten)
+            {
+                Cook(potato);
+            }
+        }
+
+        /* -------------------------- */
+
+        int x = 0;
+        int y = 0;
+        int minX = 0;
+        int maxX = 0;
+        int maxY = 0;
+        int minY = 0;
+
+        bool shoudVisitCell = true;
+        bool isInBoundsX = minX <= x && x <= maxX;
+        bool isInBoundsY = minY <= y && y <= maxY;
+
+        if (isInBoundsX && isInBoundsY && shoudVisitCell)
+        {
+            VisitCell();
+        }
+    }
+}
