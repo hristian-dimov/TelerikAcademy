00782fb874762455b13c2c8e5ca935ff4faf3fdf
Hristian Dimov <hristian.dimov@gmail.com>
2013-05-07 22:34:19 +0300

    09. AssertionsAndExceptions homework [HQC]

---
+++ HighQualityCode/09. AssertionsAndExceptions/Exceptions-Homework/ExceptionsHomework.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exceptions_Homework
+{
+    public class ExceptionsHomework
+    {
+        static void Main()
+        {
+
+            var substr = MathUtils.Subsequence("Hello!".ToCharArray(), 2, 3);
+            Console.WriteLine(substr);
+
+            var subarr = MathUtils.Subsequence(new int[] { -1, 3, 2, 1 }, 0, 2);
+            Console.WriteLine(String.Join(" ", subarr));
+
+            var allarr = MathUtils.Subsequence(new int[] { -1, 3, 2, 1 }, 0, 4);
+            Console.WriteLine(String.Join(" ", allarr));
+
+            var emptyarr = MathUtils.Subsequence(new int[] { -1, 3, 2, 1 }, 0, 0);
+            Console.WriteLine(String.Join(" ", emptyarr));
+
+            try
+            {
+                Console.WriteLine(MathUtils.ExtractEnding("I love C#", 2));
+                Console.WriteLine(MathUtils.ExtractEnding("Nakov", 4));
+                Console.WriteLine(MathUtils.ExtractEnding("beer", 4));
+                Console.WriteLine(MathUtils.ExtractEnding("Hi", 100));
+            }
+            catch (NullReferenceException nre)
+            {
+                Console.WriteLine(nre.Message);
+            }
+            catch (ArgumentOutOfRangeException aore)
+            {
+                Console.WriteLine(aore.Message);
+            }
+
+            if (MathUtils.CheckPrime(23))
+            {
+                Console.WriteLine("23 is prime.");
+            }
+            else
+            {
+                Console.WriteLine("23 is not prime");
+            }
+
+            if (MathUtils.CheckPrime(33))
+            {
+                Console.WriteLine("33 is prime.");
+            }
+            else
+            {
+                Console.WriteLine("33 is not prime");
+            }
+
+            List<Exam> peterExams = new List<Exam>()
+            {
+                new SimpleMathExam(2),
+                new CSharpExam(55),
+                new CSharpExam(100),
+                new SimpleMathExam(1),
+                new CSharpExam(0),
+            };
+
+            Student peter = new Student("Peter", "Petrov", peterExams);
+            double peterAverageResult = peter.CalcAverageExamResultInPercents();
+            Console.WriteLine("Average results = {0:p0}", peterAverageResult);
+        }
+    }
+}
---
+++ HighQualityCode/09. AssertionsAndExceptions/Exceptions-Homework/Student.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Exceptions_Homework
+{
+    public class Student
+    {
+        private string firstName;
+        private string lastName;
+        private IList<Exam> exams;
+
+        public Student(string firstName, string lastName, IList<Exam> exams)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Exams = exams;
+        }
+
+        public string FirstName
+        {
+            get 
+            { 
+                return this.firstName; 
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentNullException("First name must have almoust one charackter.");
+                }
+
+                this.firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get 
+            { 
+                return this.lastName;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentNullException("Last name must have almoust one charackter.");
+                }
+
+                this.lastName = value;
+            }
+        }
+
+        public IList<Exam> Exams
+        {
+            get 
+            { 
+                return this.exams;
+            }
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException("Exam list can't be null");
+                }
+
+                if (value.Count == 0)
+                {
+                    throw new ArgumentOutOfRangeException("Exam list must have at least one exam.");
+                }
+
+                this.exams = value;
+            }
+        }
+
+        public IList<ExamResult> CheckExams()
+        {
+            IList<ExamResult> results = new List<ExamResult>();
+            for (int i = 0; i < this.Exams.Count; i++)
+            {
+                results.Add(this.Exams[i].Check());
+            }
+
+            return results;
+        }
+
+        public double CalcAverageExamResultInPercents()
+        {
+            double[] examScore = new double[this.Exams.Count];
+            IList<ExamResult> examResults = CheckExams();
+
+            for (int i = 0; i < examResults.Count; i++)
+            {
+                examScore[i] = ((double)examResults[i].Grade - examResults[i].MinGrade) / (examResults[i].MaxGrade - examResults[i].MinGrade);
+            }
+
+            return examScore.Average();
+        }
+    }
+}
