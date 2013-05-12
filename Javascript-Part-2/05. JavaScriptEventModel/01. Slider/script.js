288c8ccc34e57fb134553f7168f4fff2887e3d2f
Hristian Dimov <hristian.dimov@gmail.com>
2013-05-12 23:37:01 +0300

    05. JavaScriptEventModel homework [JS-2]

---
+++ Javascript-Part-2/05. JavaScriptEventModel/01. Slider/script.js
@@ -0,0 +1,31 @@
+﻿(function () {
+    var leftArrow = document.getElementById("left");
+    var rightArrow = document.getElementById("right");
+    var image = document.getElementById("image");
+    var imageNumber = 1;
+    var maxImages = 4;
+
+    rightArrow.onclick = function () {
+        if (imageNumber >= maxImages) {
+            imageNumber = 1;
+        }
+        else {
+            imageNumber++;
+        }
+
+        image.src = "images/girl" + imageNumber + ".jpg";
+        return false;
+    }
+
+    leftArrow.onclick = function () {
+        if (imageNumber <= 1) {
+            imageNumber = maxImages;
+        }
+        else {
+            imageNumber--;
+        }
+
+        image.src = "images/girl" + imageNumber + ".jpg";
+        return false;
+    }
+})();
---
+++ Javascript-Part-2/05. JavaScriptEventModel/02. TODOList/script.js
@@ -0,0 +1,57 @@
+﻿(function () {
+    var addButton = document.getElementById('addButton');
+    var deleteButton = document.getElementById('deleteButton');
+    var showHideButton = document.getElementById('showHideButton');
+    var todoContainer = document.getElementById('todoContainer');
+    var uniqueID = 1;
+
+    addButton.onclick = function () {
+        var getAllListItem = document.getElementsByTagName('li');
+        if (getAllListItem.length > 0) {
+            for (var i = 0; i < getAllListItem.length; i++) {
+                getAllListItem[i].style.display = "block";
+            }
+        }
+
+        var todoText = document.getElementById('todoText').value;
+
+        var todoListItemRadioButton = document.createElement('input');
+        todoListItemRadioButton.type = 'radio';
+        todoListItemRadioButton.name = 'todoListItem';
+        todoListItemRadioButton.id = 'todoListItemID-' + uniqueID++;
+
+        var todoListItem = document.createElement("li");
+        todoListItem.appendChild(todoListItemRadioButton);
+        todoListItem.style.display = "block"
+        todoListItem.innerHTML += todoText;
+
+        todoContainer.appendChild(todoListItem);
+
+    }
+
+    deleteButton.onclick = function () {
+        var getAllListItem = document.getElementsByTagName('input');
+        var selectedRadio = {};
+
+        for (var i = 0; i < getAllListItem.length; i++) {
+            if (getAllListItem[i].type == 'radio' && getAllListItem[i].checked) {
+                selectedRadio = document.getElementById(getAllListItem[i].id);
+                todoContainer.removeChild(selectedRadio.parentNode);
+            }
+        }
+    }
+
+    showHideButton.onclick = function () {
+        var getAllListItem = document.getElementsByTagName('li');
+
+        for (var i = 0; i < getAllListItem.length; i++) {
+            if (getAllListItem[i].style.display == "block") {
+                getAllListItem[i].style.display = "none";
+            }
+            else {
+                getAllListItem[i].style.display = "block";
+            }
+        }
+    }
+
+})();
