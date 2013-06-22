67f78c9de9de1768484a3d78e61e57e58a33c083
Hristian Dimov <hristian.dimov@gmail.com>
2013-06-22 21:12:23 +0300

    09. Combinatorics [DSA]

---
+++ DataStuctures-and-Algorithms/09. Combinatorics/01. BinaryPasswords/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class Program
+{
+    static void Main()
+    {
+        string password = Console.ReadLine();
+        int count = 0;
+
+        foreach (var letter in password)
+        {
+            if (letter == '*')
+            {
+                count++;
+            }
+        }
+
+        Console.WriteLine((long)Math.Pow(2, count));
+    }
+}
---
+++ DataStuctures-and-Algorithms/09. Combinatorics/04. LineOfBalls/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+
+class Program
+{
+    static BigInteger Factorial(BigInteger n)
+    {
+        BigInteger result = 1;
+
+        for (BigInteger i = n; i > 0; i--)
+        {
+            result *= i;
+        }
+
+        return result;
+    }
+
+    static void Main()
+    {
+        Dictionary<char, int> chars = new Dictionary<char, int>();
+        string line = Console.ReadLine();
+
+        foreach (var letter in line)
+        {
+            if (!chars.ContainsKey(letter))
+            {
+                chars.Add(letter, 1);
+            }
+            else
+            {
+                chars[letter]++;
+            }
+        }
+
+        BigInteger combinations = Factorial(line.Length);
+
+
+        foreach (var color in chars)
+        {
+            combinations /= Factorial(color.Value);
+        }
+
+        Console.WriteLine(combinations);
+    }
+}
