63044bf37abe646fc2e5c9aa2755ef4c8f70e996
Hristian Dimov <hristian.dimov@gmail.com>
2013-06-24 23:50:19 +0300

    11. OtherAlgorithms [DSA]

---
+++ DataStuctures-and-Algorithms/11. OtherAlgorithms/CheckTriangle/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace CheckTriangle
+{
+    class CheckTriangle
+    {
+        static void Main()
+        {
+            double x1 = 1.0;
+            double y1 = 1.0;
+            double x2 = 2.0;
+            double y2 = 3.0;
+            double x3 = 3.0;
+            double y3 = 2.0;
+
+            double x4 = 2.0;
+            double y4 = 2.0;
+
+            double dx = (x4 - x3);
+            double dy = (y4 - y3);
+
+            double A = x1 - x3;
+            double B = y1 - y3;
+            double C = x2 - x3;
+            double D = y2 - y3;
+
+            double denominator = A * D - B * C;
+
+            double alpha = D * dx - C * dy;
+            alpha /= denominator;
+            double beta = -B * dx + A * dy;
+            beta /= denominator;
+            double gamma = 1.0 - (alpha + beta);
+
+            if (alpha >= 0 && beta >= 0 && gamma >= 0)
+            {
+                Console.WriteLine("The Point is inside the triangle");
+            }
+            else
+            {
+                Console.WriteLine("The Point is not inside the triangle");
+            }
+        }
+    }
+}
---
+++ DataStuctures-and-Algorithms/11. OtherAlgorithms/Coins/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Coins
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<int> coins = new List<int>();
+            List<int> amounts = new List<int>() { 1, 2, 5 };
+            int goal = 33;
+
+            Change(coins, amounts, 0, 0, goal);
+        }
+
+        static void Change(List<int> coins, List<int> amounts, int highest, int sum, int goal)
+        {
+            if (sum == goal)
+            {
+                Display(coins, amounts, goal);
+                return;
+            }
+
+            if (sum > goal)
+            {
+                return;
+            }
+
+            foreach (int value in amounts)
+            {
+                if (value >= highest)
+                {
+                    List<int> copy = new List<int>(coins);
+                    copy.Add(value);
+                    Change(copy, amounts, value, sum + value, goal);
+                }
+            }
+        }
+
+        static void Display(List<int> coins, List<int> amounts, int goal)
+        {
+            int total = 0;
+
+            foreach (int amount in amounts)
+            {
+                int count = coins.Count(value => value == amount);
+                Console.Write("{0} coins x {1}", count, amount);
+
+                total++;
+                if (total != amounts.Count)
+                {
+                    Console.Write(" + ");
+                }
+                else
+                {
+                    Console.Write(" = {0}", goal);
+                }
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
