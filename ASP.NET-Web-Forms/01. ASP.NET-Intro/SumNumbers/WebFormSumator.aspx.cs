f1beaadee30f12de6db219195afd85de508aad3a
Hristian Dimov <hristian.dimov@gmail.com>
2013-09-08 23:48:22 +0300

    01. ASP.NET-Intro [ASP-WebForms]

---
+++ ASP.NET-Web-Forms/01. ASP.NET-Intro/SumNumbers/WebFormSumator.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SumNumbers
+{
+    public partial class WebFormSumator : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        public void ButtonCalculateSum_Click(object sender, EventArgs e)
+        {
+            decimal firstNum = decimal.Parse(this.TextBoxFirstNum.Text);
+            decimal secondNum = decimal.Parse(this.TextBoxSecondNum.Text);
+
+            decimal result = firstNum + secondNum;
+
+            this.TextBoxSum.Text = result.ToString();
+        }
+    }
+}
